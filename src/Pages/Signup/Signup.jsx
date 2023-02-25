cd8668e1daba0aee6708639bd9ac1625e553dec2
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2023-02-25 12:12:22 +0000

    set state

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Signup.scss"
 
 const Signup = () => {
+  const [userDetails, setUserDetails] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
   return (
     <div className="signup">
       <form>
         <div className="label-logic">
           <label htmlFor="firstName">First Name :</label>
-          <input id="first-name" name="firstName" placeholder="Enter your first name" />
+          <input id="first-name" name="firstName" placeholder="Enter your first name"  required/>
         </div>
         <div className="label-logic">
           <label htmlFor="LastName">Last Name :</label>
-          <input id="last-name" name="LastName" placeholder="Enter your last name" />
+          <input id="last-name" name="LastName" placeholder="Enter your last name" required />
         </div>
         <div className="label-logic">
           <label htmlFor="email">Email :</label>
-          <input id="email" name="email" placeholder="Enter your email" />
+          <input id="email" name="email" placeholder="Enter your email" required />
         </div>
         <div className="label-logic">
           <label htmlFor="password">Password :</label>
@@ -24,6 +31,7 @@ const Signup = () => {
             name="password"
             type="password"
             placeholder="Enter your password"
+            required
           />
         </div>
         <div className="label-logic">
@@ -33,9 +41,11 @@ const Signup = () => {
             name="confirm-password"
             type="password"
             placeholder="confirm password"
+            required
           />
         </div>
-        <input type="submit" placeholder="Submit" />
+        <input type="submit" placeholder="Submit" className="submit-btn"/>
+        <span className="login-link">already have an account login</span>
       </form>
     </div>
   );
