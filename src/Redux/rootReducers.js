8f8f44d179c5db54eead036d96c6186d847004b3
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2023-03-03 20:46:04 +0000

    fixs

--- src/Redux/rootReducers.js
+++ src/Redux/rootReducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { allItemsApi } from "./Services/Services";
-
 export const rootReducer = combineReducers({
-    getAllItems: allItemsApi.reducer,
+
+    [allItemsApi.reducerPath]: allItemsApi.reducer
 })
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { rootReducer } from "./rootReducers";
+import { allItemsApi } from "./Services/Services";
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(rootReducer.middleware),
+    getDefaultMiddleware().concat(allItemsApi.middleware),
 });
 
 setupListeners(store.dispatch);
